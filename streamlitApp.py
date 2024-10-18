6991fe793ec8eae01ee7aa46c57a6271b413b54a
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-18 22:30:34 +0100

    Update streamlitApp.py
    Adding new streamlit elements to my app

--- streamlitApp.py
+++ streamlitApp.py
@@ -1,2 +1,7 @@
 import streamlit as st
-st.write('Hello world!')
+
+st.title('My First Streamlit App')
+st.write('Hello my friend!')
+
+user_input = st.text_input("Enter some text")
+st.write('The user entered:', user_input)
