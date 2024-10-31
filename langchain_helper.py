6aaf42f3fed57536c34ac9039f461b00f7614799
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-31 23:24:09 +0100

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -4,10 +4,10 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 import google.generativeai as genai
 from secret_key import GOOGLE_API_KEY
 
-import os
+from dotenv import load_dotenv
 
-GOOGLE_API_KEY=userdata.get('GOOGLE_API_KEY')
-genai.configure(api_key=GOOGLE_API_KEY)
+
+load_dotenv()
 
 # Initialize the Gemini model
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-002",temperature=0.7, google_api_key=GOOGLE_API_KEY)
