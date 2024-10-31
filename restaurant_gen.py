ee9c2399102d9f6517b5d5223bf57941608be49b
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-01 00:41:31 +0100

    Update restaurant_gen.py

--- restaurant_gen.py
+++ restaurant_gen.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import langchain_helper
-from dotenv import load_dotenv
 
-
-load_dotenv()
 
 st.title("Restaurant Name Generator")
 cuisine = st.sidebar.selectbox("Pick a Cuisine", ("Indian", "Italian","Nigerian", "Ghanian", "American", "Mexican"))
