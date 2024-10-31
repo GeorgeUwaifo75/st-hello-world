93f03a8d3efdd64d3f2f30d8c854d6f97a3aea29
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-31 22:25:55 +0100

    Update restaurant_gen.py

--- restaurant_gen.py
+++ restaurant_gen.py
@@ -10,8 +10,8 @@ def generate_restaurant_name_and_items(cuisine):
 
 if cuisine:
   response = generate_restaurant_name_and_items(cuisine)
-  st.header(response[restaurant_name])
-  menu_items = response[menu_items].split(",")
+  st.header(response(restaurant_name))
+  menu_items = response(menu_items).split(",")
   
   st.write("** Menu Items**")
   for item in menu_items:
