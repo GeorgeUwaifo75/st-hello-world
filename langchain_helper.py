0a271b146a58f4d706bcca5b1affc5a50a2bcafb
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-01 00:42:32 +0100

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -6,18 +6,20 @@ from langchain.chains import LLMChain
 from langchain.chains import SequentialChain
 
 import google.generativeai as genai
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 
-#load_dotenv()
+
 
 # Initialize the Gemini model
 #llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-002",temperature=0.7, google_api_key=GOOGLE_API_KEY)
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-002",temperature=0.7)
+#llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-002",temperature=0.7)
 
 
 
 def generate_restaurant_name_and_items(cuisine):
+    load_dotenv()
+    llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-002",temperature=0.7)
     prompt_template_name = PromptTemplate(
     input_variables=["cuisine"], 
     template="I want to open a restaurant for {cuisine} food. Can you suggest one fancy name?")
