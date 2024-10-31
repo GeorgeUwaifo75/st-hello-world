83421323323c395995d8bc017dc21305335e8f42
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-31 22:23:31 +0100

    Update restaurant_gen.py

--- restaurant_gen.py
+++ restaurant_gen.py
@@ -1,3 +1,18 @@
 import streamlit as st
-st.header("Restaurant Generator")
-st.sidebar.selectbox("Pick a Cuisine", ("India", "Italian","Nigerian", "Ghanian", "American", "Mexican"))
+st.header("Restaurant Name Generator")
+cuisine = st.sidebar.selectbox("Pick a Cuisine", ("India", "Italian","Nigerian", "Ghanian", "American", "Mexican"))
+
+def generate_restaurant_name_and_items(cuisine):
+  return{
+    'restaurant_name':'Curry Delight',
+    'menu_items':'Samosa, paneer, tikka'
+  }
+
+if cuisine:
+  response = generate_restaurant_name_and_items(cuisine)
+  st.header(response[restaurant_name])
+  menu_items = response[menu_items].split(",")
+  
+  st.write("** Menu Items**")
+  for item in menu_items:
+    st.write("-",item)
