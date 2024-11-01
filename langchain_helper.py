8811293f0b53a4a5bf6ebc7d78645e5729ec99b5
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-01 02:06:19 +0100

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 # Initialize the Gemini model
 #llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-002",temperature=0.7, google_api_key=GOOGLE_API_KEY)
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-002",temperature=0.7)
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash",temperature=0.7)
 
 
 def generate_restaurant_name_and_items(cuisine):
