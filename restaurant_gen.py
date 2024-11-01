79f030d174ab9efd4e6cccb6dbb5c4c54d0e1215
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-01 01:50:48 +0100

    Update restaurant_gen.py

--- restaurant_gen.py
+++ restaurant_gen.py
@@ -7,19 +7,18 @@ cuisine = st.sidebar.selectbox("Pick a Cuisine", ("Indian", "Italian","Nigerian"
 
 
 if cuisine:
-  #response = langchain_helper.generate_restaurant_name_and_items(cuisine)
-  pass
+  response = langchain_helper.generate_restaurant_name_and_items(cuisine)
+ 
  
   
 # Define keys explicitly
-  restaurant_name_key = 'restaurant'
-  menu_items_key = 'menu_items'
+#  restaurant_name_key = 'restaurant'
+#  menu_items_key = 'menu_items'
 
- # st.header(response[restaurant_name_key])
-  #menu_items = response[menu_items_key].split(",")
+#  st.header(response[restaurant_name_key])
+#  menu_items = response[menu_items_key].split(",")
 
-  
-  
-  st.write("** Menu Items**")
-  for item in menu_items:
-      st.write("-",item)
+
+ # st.write("** Menu Items**")
+#  for item in menu_items:
+#     st.write("-",item)
