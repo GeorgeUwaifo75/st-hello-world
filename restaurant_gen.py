0740adfaf2ee5bf87d45361222162a902de039a4
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-01 01:51:53 +0100

    Update restaurant_gen.py

--- restaurant_gen.py
+++ restaurant_gen.py
@@ -12,13 +12,13 @@ if cuisine:
  
   
 # Define keys explicitly
-#  restaurant_name_key = 'restaurant'
-#  menu_items_key = 'menu_items'
+  restaurant_name_key = 'restaurant'
+  menu_items_key = 'menu_items'
 
-#  st.header(response[restaurant_name_key])
-#  menu_items = response[menu_items_key].split(",")
+  st.header(response[restaurant_name_key])
+  menu_items = response[menu_items_key].split(",")
 
 
- # st.write("** Menu Items**")
-#  for item in menu_items:
-#     st.write("-",item)
+  st.write("** Menu Items**")
+  for item in menu_items:
+     st.write("-",item)
