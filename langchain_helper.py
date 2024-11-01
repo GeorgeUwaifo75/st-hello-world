c76dd324df3c9cd02b7107f2573c4ef83a237613
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-01 01:35:51 +0100

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -18,21 +18,21 @@ llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-002",temperature=0.7)
 def generate_restaurant_name_and_items(cuisine):
    
    prompt_template_name = PromptTemplate(
-   input_variables=["cuisine"], 
-   template="I want to open a restaurant for {cuisine} food. Can you suggest one fancy name?")
+      input_variables=["cuisine"], 
+      template="I want to open a restaurant for {cuisine} food. Can you suggest one fancy name?")
 
    name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key ="restaurant" )
 
    prompt_template_items = PromptTemplate(
-   input_variables=["restaurant"], 
-   template="Suggest to me some food menu for the restaurant {restaurant}. Return it as a comma separate list.")
+      input_variables=["restaurant"], 
+      template="Suggest to me some food menu for the restaurant {restaurant}. Return it as a comma separate list.")
 
    food_items_chain = LLMChain(llm=llm, prompt=prompt_template_items, output_key ="menu_items")
 
    chain = SequentialChain(
-   chains=[name_chain, food_items_chain],
-   input_variables=["cuisine"],
-   output_variables=["restaurant", "menu_items"]
+      chains=[name_chain, food_items_chain],
+      input_variables=["cuisine"],
+      output_variables=["restaurant", "menu_items"]
    )
 
    response = chain({"cuisine": cuisine})
