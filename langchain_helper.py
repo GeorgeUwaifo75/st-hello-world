be9efcd8cb4adc6b11e53b0231c287f93234db79
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-01 14:08:50 +0100

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -21,7 +21,7 @@ def generate_restaurant_name_and_items(cuisine):
       input_variables=["cuisine"], 
       template="I want to open a restaurant for {cuisine} food. Can you suggest one fancy name?")
 
-   name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key ="restaurant_name" )
+   name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key ="restaurant" )
 
    prompt_template_items = PromptTemplate(
       input_variables=["restaurant"], 
@@ -29,12 +29,17 @@ def generate_restaurant_name_and_items(cuisine):
 
    food_items_chain = LLMChain(llm=llm, prompt=prompt_template_items, output_key ="menu_items")
 
+   
+   response = chain({'cuisine': cuisine})
+
    chain = SequentialChain(
-      chains=[name_chain, food_items_chain],
-      input_variables=["cuisine"],
-      output_variables=["restaurant_name", "menu_items"]
+    chains=[name_chain, food_items_chain],
+    input_variables=["cuisine"],
+    output_variables=["restaurant", "menu_items"]
    )
 
-   response = chain({'cuisine': cuisine})
+   response = chain.invoke({"cuisine": "Indian"})
+   print(response)
+   
    
    return response
